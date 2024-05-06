292127145b726a9d6486430b2b38f12a0a51e03f
Tari20691 <150802689+Tari20691@users.noreply.github.com>
2024-05-06 15:10:42 +0200

    Added rel="noreferrer" for React links

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ return ( <div className="App"> <h1>Weather Serach Engine</h1>
 <SearchEngine />
 <br />
 <footer>
-    Coded by <a href="https://github.com/Tari20691" target="_blank" title="Annas GitHub">Anna Tari',</a>
-    is open-sourced on <a href="https://github.com/Tari20691/weather-react-week4" target="_blank" title="GitHub project">GitHub</a>
-    &nbsp;and hosted on <a href="https://shecodesreactweek4.netlify.app" target="_blank" title="Netlify">Netlify.</a>
+    Coded by <a href="https://github.com/Tari20691" target="_blank" rel="noreferrer" title="Annas GitHub">Anna Tari',</a>
+    is open-sourced on <a href="https://github.com/Tari20691/weather-react-week4" rel="noreferrer" target="_blank" title="GitHub project">GitHub</a>
+    &nbsp;and hosted on <a href="https://shecodesreactweek4.netlify.app" rel="noreferrer" target="_blank" title="Netlify">Netlify.</a>
 
 </footer>
 </div>
